29f3b2566b2e854cd78dbfa6f2eccd1004447c24
MarcoFiesque <marco.fiesque@gmail.com>
2025-11-23 18:14:48 +0100

    php and nginx services built

---
+++ lemp_stack_compose/src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lemp container with Docker Compose</title>
+    <style>
+        .center{
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+    </style>
+</head>
+<body>
+    <img src="https://tech.osteel.me/images/2020/03/04/hello.gif" alt="Hello there" class="center">
+</body>
+</html>
